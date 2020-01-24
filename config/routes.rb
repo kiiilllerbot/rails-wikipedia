c8ed11b36479224f3080e75c02e3c0781d0e711f
Shimol-Khan <se.shimolkhan@gmail.com>
2020-01-25 01:40:06 +0800

    Implemented User Authentication Module

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  	devise_for :users
+  	
+  	devise_scope :user do
+	  get '/users', to: 'devise/registrations#new'
+	  get '/users/password', to: 'devise/passwords#new'
+	end
+
 end
+
+
