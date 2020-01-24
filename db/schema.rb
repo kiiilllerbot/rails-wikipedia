b73dfc59372d5be2551ce6cb807558629ffd9a2d
Shimol-Khan <se.shimolkhan@gmail.com>
2020-01-25 01:50:07 +0800

    Generated Category Model And Seeded Data

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_24_173412) do
+ActiveRecord::Schema.define(version: 2020_01_24_174122) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
